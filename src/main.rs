ac8557cc0e66f37f337bea33a74e4be95db7d3d9
Jamie Dabu <agonistagent@gmail.com>
2020-12-10 02:06:48 -0600

    Yeeted dif

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     let mut jolts = max + 3;
-    let mut dif = [0,0,0];
     let mut chain = Vec::new();
     let mut vstack = Vec::new();
     vstack.push(3);
+    adapters.push(0);
     while jolts > 3 || !adapters.is_empty() {
         match vstack.pop() {
-            Some(0) => {dif[0] -= 1; adapters.insert(chain.pop().unwrap());}, //expect = panic if chain is empty
+            Some(0) => { adapters.insert(chain.pop().unwrap());}, //expect = panic if chain is empty
             Some(i) => {
                 vstack.push(i-1);
                 if jolts >= i && adapters.remove(&(jolts - i)) {
-                    if i < 3 {
-                        dif[i] -= 1;
-                    }
                     chain.push(jolts-i);
                     jolts -= i;
                     vstack.push(3);
-                    dif[i-1] += 1;
                 }
             }
             None => panic!("Empty vstack"),
         }
     }
+
+    let dif: [usize; 3] = [0,0,0];
+    let mut last = chain[0];
+    for i in chain {
+        if i != last {
+            dif[last-i-1];
+            last = i;
+        }
+    }
+
     println!("{}",dif[0] * dif[2]);
     Ok(())
 }
