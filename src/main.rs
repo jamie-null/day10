43d85acef6c93aadfab0f5936df2c14b921bddd0
Jamie Dabu <agonistagent@gmail.com>
2020-12-15 17:11:12 -0600

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::cmp;
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap};
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -9,75 +9,47 @@ fn main() -> Result<(), Box<dyn Error>> {
     let buf = BufReader::new(raw);
 
     let mut max = 0;
-    let mut adapters = HashSet::new();
+    let mut adapters = Vec::new();
     for line in buf.lines() {
         let val = line.unwrap().parse::<usize>()?;
         max = cmp::max(max, val);
-        if !adapters.insert(val) {
-            panic!("Duplicate adapters detected");
-        }
+        adapters.push(val);
     }
-    println!("{:?}", adapters);
-    let mut chain = Vec::new();
-    let mut vstack = Vec::new();
-    chain.push(max + 3);
-    vstack.push(3); //3 step drop from device
-    adapters.insert(0); //wall
-    let mut jolts = max + 3;
-    //while jolts > 3 || !adapters.is_empty() {
-    /*
+    adapters.push(max + 3);
+    adapters.push(0);
+    adapters.sort();
 
-    */
-    let mut combos: HashMap<usize, usize> = HashMap::new();
-    combos.insert(0, 1);
-    while chain.len() > 1 || *vstack.last().unwrap() != 4 {
-        /*
-                println!("{}",jolts);
-                println!("{:?}", chain);
-                println!("{:?}",vstack);
-
-                if jolts <= 3 {
-                    combos.insert()
-                }
-        */
-        match vstack.pop() {
-            Some(i) if i >= 4 || i > jolts => {
-                adapters.insert(chain.pop().unwrap());
-                jolts = *chain.last().unwrap();
-            } //expect = panic if chain is empty
-            Some(i) => {
-                vstack.push(i + 1);
-                if adapters.remove(&(jolts - i)) {
-                    if let Some(c) = combos.get(&(jolts - i)) {
-                        let c = c.clone();
-                        if let Some(current) = combos.get_mut(&jolts) {
-                            *current += c;
-                        } else {
-                            combos.insert(jolts,c);
-                        }
-                        adapters.insert(jolts-i);
-                    } else {
-                        chain.push(jolts - i);
-                        jolts -= i;
-                        vstack.push(1);
-                    }
-                }
-            }
-            None => panic!("Empty vstack"),
-        }
+    let mut graph: BTreeMap<usize,Vec<usize>> = BTreeMap::new();
+    for jolts in &adapters {
+        let next = (1..4).filter(|n| adapters.contains(&(n + jolts)))
+                         .map(|n| n + jolts).collect();
+        //println!("{:#?}",next);
+        graph.insert(*jolts, next);
     }
-    println!("{}", combos.get(&(max)).unwrap());
-    /*
-    let mut dif: [usize; 3] = [0,0,0];
-    let mut last = chain[0];
-    for i in chain {
-        if i != last {
-            dif[last-i-1] += 1;
-            last = i;
-        }
-    }
-
-    println!("{:?}",dif);
-    */
+    let mut combos = BTreeMap::new();
+    println!("{}",getcombos(0,&mut graph,&mut combos,max));
     Ok(())
+}
+
+fn getcombos(jolts: usize, graph: &mut BTreeMap<usize,Vec<usize>>, combos: &mut BTreeMap<usize,usize>, max: usize) -> usize {
+    if jolts == max + 3 {
+        1
+    } else {
+        let mut sum = 0;
+        let next = match graph.get(&jolts) {
+            Some(a) => a,
+            _ => {println!("{}",jolts); unreachable!()},
+        };
+        for next in next.clone() {
+            let next = next;
+            if let Some(c) = combos.get(&next) {
+                sum += c;
+            } else if graph.contains_key(&next) {
+                let c = getcombos(next,graph,combos,max);
+                sum += c;
+                combos.insert(next, c);
+            }
+        }
+        sum
+    }
 }
